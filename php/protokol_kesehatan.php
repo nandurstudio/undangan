333f87c1a4c9dfc85f4b3a0b13e568f3ec452cff
Nandang Duryat <nandang.dhe@gmail.com>
2022-03-10 04:37:25 +0700

    Fix cover, tambah sesi, protokol kesehatan, quote.

---
+++ php/protokol_kesehatan.php
@@ -0,0 +1,63 @@
+    <div class="row">
+        <div style="text-align:center">
+            <p>Acara ini dilaksanakan dengan menerapkan</br>
+                PROTOKOL KESEHATAN</br>
+                Sebagai berikut :</p>
+        </div>
+
+        <div class="icon-content">
+            <div class="col-md-3 col-sm-6 animate-box">
+                <div class="feature-center">
+                    <span class="icon">
+                        <img width="75%" height="75%" src="https://kondangankuy.com/wp-content/uploads/2021/01/masker.png">
+                    </span>
+                    <span class="counter-label">Memakai Masker</span>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 animate-box">
+                <div class="feature-center">
+                    <span class="icon">
+                        <img width="100%" height="100%" src="https://kondangankuy.com/wp-content/uploads/2021/01/cuci-tangan.png">
+                    </span>
+                    <span class="counter-label">Mencuci Tangan</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 animate-box">
+                <div class="feature-center">
+                    <span class="icon">
+                        <img width="100%" height="100%" src="https://kondangankuy.com/wp-content/uploads/2021/01/jarak.png">
+                    </span>
+                    <span class="counter-label">Menjaga Jarak</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 animate-box">
+                <div class="feature-center">
+                    <span class="icon">
+                        <img width="100%" height="100%" src="https://kondangankuy.com/wp-content/uploads/2021/01/jabat-tangan.png">
+                    </span>
+                    <span class="counter-label">Tidak Berjabat Tangan</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 animate-box">
+                <div class="feature-center">
+                    <span class="icon">
+                        <img width="100%" height="100%" src="https://kondangankuy.com/wp-content/uploads/2021/01/suhu.png">
+                    </span>
+                    <span class="counter-label">Mengecek Suhu Tubuh</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 animate-box">
+                <div class="feature-center">
+                    <span class="icon">
+                        <img width="100%" height="100%" src="https://kondangankuy.com/wp-content/uploads/2021/01/handsanitizer.png">
+                    </span>
+                    <span class="counter-label">Disarankan Membawa Handsanitizer</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <p>Tanpa mengurangi rasa hormat demi mengurangi penyebaran pandemi, mohon untuk tetap mengikuti protokol kesehatan dan datang tepat waktu sesuai dengan jam yang sudah tertera di undangan.</p>
+
+    <p>Tekan tombol <span class="pinkish">(X)</span> pada kanan atas untuk menutup informasi ini / informasi ini akan tertutup otomatis setelah 15 detik.</p>
