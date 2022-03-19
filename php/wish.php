e0b01d129043d1e0f01a2a0ae54aca019f82a1f9
Nandang Duryat <nandang.dhe@gmail.com>
2022-03-19 18:09:14 +0700

    Reservasi, estimated guest, wish and do'a

--- php/wish.php
+++ php/wish.php
@@ -1,5 +1,7 @@
 <?php
 include 'connection.php';
+$shout_query = "SELECT * FROM tr_ucapan ORDER BY ucapanId DESC";
+$shouts = mysqli_query($conn, $shout_query);
 // $Submit = isset($_POST['Submit']) ? $_POST['Submit'] : false;
 // $Name = isset($_POST['Name']) ? $_POST['Name'] : '';
 // $Ucapan = isset($_POST['Ucapan']) ? $_POST['Ucapan'] : '';
@@ -20,6 +22,5 @@ $wish = isset($_POST['wish']) ? $_POST['wish'] : '';
 
 mysqli_query($conn, "SELECT * FROM tr_ucapan");
 mysqli_query($conn, "INSERT INTO `tr_ucapan` (`ucapanId`, `date`, `nama`, `ucapan`) VALUES (NULL, current_timestamp(), '$name', '$wish');");
-
-// header("Location:/auditonline/html/users.html");
+//header("Location:/undangan/php/wish.php");
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ebd98fd03eba498038a18bab9e5898ac685abe1d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ebd98fd03eba498038a18bab9e5898ac685abe1d',
+            'dev_requirement' => false,
+        ),
+        'jamesmoss/flywheel' => array(
+            'pretty_version' => '0.6.0',
+            'version' => '0.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jamesmoss/flywheel',
+            'aliases' => array(),
+            'reference' => '001a7bba01ad537d119853cd86078b1b51540daf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
