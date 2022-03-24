fd50d64497f9d0bf5ed3bb588458c9310aa66b7b
Nandang Duryat <nandang.dhe@gmail.com>
2022-03-24 11:39:25 +0700

    Fix scan tamu dan QR code bug %26

---
+++ jumlahtamu.php
@@ -0,0 +1,9 @@
+<?php
+include './php/connection.php';
+$text = $_POST['text'];
+$selectquery = "SELECT * FROM tr_tamu ORDER BY tamuId DESC LIMIT 1";
+$reservasi = mysqli_query($conn, $selectquery);
+$row = $reservasi->fetch_assoc();
+$id_guest = $row['tamuId'];
+$update_query = "UPDATE `tr_tamu` SET `jumlah_tamu` = '$text' WHERE `tr_tamu`.`tamuId` = '$id_guest';";
+mysqli_query($conn, $update_query);
