47feadd3c2da4a8fcfc806e88a5d349f77b071b9
Nandang Duryat <nandang.dhe@gmail.com>
2022-03-26 14:53:55 +0700

    Add couples png and liveguestcount

---
+++ liveguestcount.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<?php
+include "./php/header.php";
+?>
+
+<body>
+	<div class="fh5co-loader"></div>
+	<div id="page">
+
+
+		<div id="fh5co-couple" style="background-image: url(images/gallery-8.jpg); background-size: cover;">
+			<div class="container">
+
+				<div class="row animate-box fadeInUp animated-fast">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+
+						<div class="col-md-3 col-sm-6 animate-box fadeInUp animated-fast">
+							<div class="feature-center">
+								<span class="icon">
+									<i class="icon-users"></i>
+								</span>
+
+							</div>
+						</div>
+						<h2 id="guest-count-icon">0 invited guests in attendance</h2>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+						<style>
+							.mySlides {
+								display: none;
+							}
+						</style>
+						<div class="col-md-8 col-md-offset-2 text-center">
+							<p>This event is supported by:</p>
+							<!-- <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p> -->
+						</div>
+
+						<div class="w3-content w3-section" style="max-width:500px">
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>CPP</h3>
+									<p><a href="https://www.instagram.com/noor.diansyah/" target="blank">@noor.diansyah</a></p>
+									<a href="https://www.instagram.com/noor.diansyah/" target="blank">
+										<img src="./images/pengantin_pria_nft.png" width="50%" height="50%">
+									</a>
+								</div>
+							</div>
+
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>CPW</h3>
+									<p><a href="https://www.instagram.com/hilyadina_/" target="blank">@hilyadina_</a></p>
+									<a href="https://www.instagram.com/hilyadina_/" target="blank">
+										<img src="./images/pengantin_wanita_nft.png" width="50%" height="50%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Photography</h3>
+									<p><a href="https://www.instagram.com/mwaafi/" target="blank">@mwaafi</a></p>
+									<a href="https://www.instagram.com/mwaafi/" target="blank">
+										<img src="./images/mwaafi.png" width="75%" height="75%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Wedding MC</h3>
+									<p><a href="https://www.instagram.com/edoalfarez/" target="blank">@edoalfarez</a></p>
+									<a href="https://www.instagram.com/edoalfarez/" target="blank">
+										<img src="./images/edoalfrez.png" width="75%" height="75%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Decoration and Attire</h3>
+									<p><a href="https://www.instagram.com/baskara_wedding_planner/" target="blank">@baskara_wedding_planner</a></p>
+									<a href="https://www.instagram.com/baskara_wedding_planner/" target="blank">
+										<img src="./images/baskara.jpeg" width="100%" height="100%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>MUA</h3>
+									<p><a href="https://www.instagram.com/armeitarivaldi/" target="blank">@armeitarivaldi</a></p>
+									<a href="https://www.instagram.com/armeitarivaldi/" target="blank">
+										<img src="./images/armeita.jpeg" width="100%" height="100%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Catering</h3>
+									<p><a href="https://www.instagram.com/teman.catering/" target="blank">@teman.catering</a></p>
+									<a href="https://www.instagram.com/teman.catering/" target="blank">
+										<img src="./images/teman_catering.png" width="100%" height="100%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Wedding Organizer</h3>
+									<p><a href="https://www.instagram.com/teman.event/" target="blank">@teman.event</a></p>
+									<a href="https://www.instagram.com/teman.event/" target="blank">
+										<img src="./images/teman_wo.png" width="100%" height="100%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Drinks</h3>
+									<p><a href="https://www.instagram.com/ruangketiga_/" target="blank">@ruangketiga_</a></p>
+									<a href="https://www.instagram.com/ruangketiga_/" target="blank">
+										<img src="./images/ruang_ketiga.png" width="75%" height="75%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Music & Entertaiment</h3>
+									<p><a href="https://www.instagram.com/dmtmusicentertaiment/" target="blank">@dmtmusicentertaiment</a></p>
+									<a href="https://www.instagram.com/dmtmusicentertaiment/" target="blank">
+										<img src="./images/dmt.png" width="75%" height="75%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Printed Invitation Card</h3>
+									<p><a href="https://www.instagram.com/sakinapercetakan/" target="blank">@sakinapercetakan</a></p>
+									<a href="https://www.instagram.com/sakinapercetakan/" target="blank">
+										<img src="./images/sakina.png" width="75%" height="75%">
+									</a>
+								</div>
+							</div>
+							<div class="feature-left mySlides w3-animate-left">
+								<span class="icon">
+									<i class="icon-instagram"></i>
+								</span>
+								<div class="feature-copy">
+									<h3>Support & Digital Invitation</h3>
+									<p><a href="https://www.instagram.com/nandur.studio/" target="blank">@nandur.studio</a></p>
+									<a href="https://www.instagram.com/nandur.studio/" target="blank">
+										<img src="./images/nandur_studio.png" width="75%" height="75%">
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 animate-box fadeInUp animated-fast">
+						<div class="fh5co-video fh5co-bg" style="background-image: url(images/img_bg_3.jpg); ">
+
+
+							<div id="yt-wrap">
+								<!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+								<div id="ytplayer"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<p>This event is carried out by implementing the health protocols.</br>
+				<p>Without reducing respect, in order to reduce the spread of the pandemic, please always follow the health protocols and arrive on time according to the hours listed on the invitation.</p>
+			</div>
+		</div>
+
+		<script type="text/javascript">
+			window.addEventListener('load', function() {
+				setInterval(function() {
+					$.ajax({ //create an ajax request to display.php
+						type: "GET",
+						url: "./php/jumlahtamurealtime.php",
+						dataType: "html", //expect html to be returned                
+						success: function(response) {
+							$("#guest-count-icon").text(response + " invited guests in attendance");
+						}
+					});
+				}, 2000);
+
+			});
+
+			// 2. This code loads the IFrame Player API code asynchronously.
+			var tag = document.createElement('script');
+			tag.src = "https://www.youtube.com/player_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+			// 3. This function creates an <iframe> (and YouTube player)
+			//    after the API code downloads.
+			var player;
+
+			function onYouTubePlayerAPIReady() {
+				player = new YT.Player('ytplayer', {
+					width: '100%',
+					height: '450px',
+					videoId: 'iKxRWdiWUYI',
+					events: {
+						'onReady': onPlayerReady,
+						'onStateChange': onPlayerStateChange
+					}
+				});
+			}
+
+			// 4. The API will call this function when the video player is ready.
+			function onPlayerReady(event) {
+				event.target.playVideo();
+				player.mute(); // comment out if you don't want the auto played video muted
+			}
+
+			// 5. The API calls this function when the player's state changes.
+			//    The function indicates that when playing a video (state=1),
+			//    the player should play for six seconds and then stop.
+			function onPlayerStateChange(event) {
+				if (event.data == YT.PlayerState.ENDED) {
+					player.seekTo(0);
+					player.playVideo();
+				}
+			}
+
+			function stopVideo() {
+				player.stopVideo();
+			}
+
+
+			var myIndex = 0;
+			carousel();
+
+			function carousel() {
+				var i;
+				var x = document.getElementsByClassName("mySlides");
+				for (i = 0; i < x.length; i++) {
+					x[i].style.display = "none";
+				}
+				myIndex++;
+				if (myIndex > x.length) {
+					myIndex = 1
+				}
+				x[myIndex - 1].style.display = "block";
+				setTimeout(carousel, 5000);
+			}
+		</script>
+
+		<!-- Footer start -->
+		<?php
+		include "./php/footer.php";
+		?>
+		<!-- Footer end -->
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- Main -->
+	<script src="js/main.js"></script>
+</body>
+
+</html>
