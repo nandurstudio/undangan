e8e8af51df2ae9f08b155f52e8b537c41e750034
Nandang Duryat <nandang.dhe@gmail.com>
2023-10-03 13:04:53 +0700

    Membuat versi Pandawa Awards 2023 Dibuatkan untuk event Pandawa Awards 2023

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2022,
-                month: 5,
-                day: 25,
-                hours: 15,
+                year: 2023,
+                month: 10,
+                day: 4,
+                hours: 16,
                 minutes: 0,
                 seconds: 0,
                 words: {
